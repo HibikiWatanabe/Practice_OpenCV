b1147a690ad8b5f496337e850406d1aeb19b795e
Chiyoko91 <hanamaruc77@gmail.com>
2021-05-12 17:46:49 +0900

    test_cvを実行するとグレースケールなtest.jpgがウィンドウ出力されます

---
+++ test_cv.py
@@ -0,0 +1,8 @@
+import numpy as np
+import cv2
+
+#テスト画像の出力をやってみる
+img = cv2.imread(r'Practice_OpenCV/test.jpg', 0) #画像読み込み
+cv2.imshow('window', img) #画像のウィンドウ表示
+cv2.waitKey(0) #キー入力待ち(この場合何かしらキーが入力されるまで無限に待つ)
+cv2.destoryAllWindows() #ここまでに作られたすべてのウィンドウを閉じる
