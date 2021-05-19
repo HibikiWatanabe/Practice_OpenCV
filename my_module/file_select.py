d510bda59682f5fab57d9a3cd08f0901b74d6a13
Chiyoko91 <hanamaruc77@gmail.com>
2021-05-19 18:28:27 +0900

    モジュール開発に着手

---
+++ my_module/file_select.py
@@ -0,0 +1,32 @@
+#ファイルを選択する機能
+import os
+import tkinter
+import tkinter.messagebox
+import tkinter.filedialog
+
+root = tkinter.Tk()
+root.withdraw()
+
+def f_select():
+    #jpgファイルのみ表示
+    typ = [("jpgファイル","*.jpg"), ("pngファイル","*.png"), ("gifファイル","*.gif")] 
+
+    #とりあえずこのファイルと同じディレクトリをiDirに格納
+    dir = os.path.abspath(os.path.dirname('..//'+__file__)) 
+
+    #ファイル選択を促すメッセージ
+    tkinter.messagebox.showinfo('mosaic_app','処理ファイルを選択してください') 
+
+    #エクスプローラによるファイル選択とfileへのパスの格納 iDirに名が入ったディレクトリが最初に開かれる
+    file = tkinter.filedialog.askopenfilename(filetypes = typ,initialdir = dir) 
+
+    # 処理ファイル名のメッセージ出力
+    tkinter.messagebox.showinfo('mosaic_app',file+'を選択しました')
+
+    return file
+
+#テスト実行用
+'''
+f_name = f_select()
+print(f_name)
+'''
--- test_cv.py
+++ test_cv.py
@@ -3,7 +3,7 @@ import cv2
 import os
 
 #テスト画像の出力をやってみる
-current_d = os.getcwd()
+
 img = cv2.imread('test.jpg', 0) #画像読み込み
 cv2.imshow('window', img) #画像のウィンドウ表示
 cv2.waitKey(0) #キー入力待ち(この場合何かしらキーが入力されるまで無限に待つ)
